4d59c9e0d5902d40b5e16ef8d8913338e5da27d9
Bass Neel <maps.bassneel@gmail.com>
2023-04-23 22:54:16 -0400

    updated bassmap.py file and requirements.txt

--- bassmap/bassmap.py
+++ bassmap/bassmap.py
@@ -1,19 +1,23 @@
 """Main module."""
 
-
-import csv
-import folium
-import branca
-import random
-import leafmap
+import requests
+import zipfile
+import io
+import json
 import geopandas as gpd
-import pandas as pd
-from shapely.geometry import Point
+import ipyleaflet
 
-from ipyleaflet import Map, GeoJSON, LayerGroup, basemaps, basemap_to_tiles, LegendControl
-from branca.colormap import linear
+from ipyleaflet import Map, TileLayer, basemaps, GeoJSON, LayersControl, ImageOverlay
+import rasterio
+import rasterio.plot
+from rasterio.plot import show
+import numpy as np
+import matplotlib.pyplot as plt
+import base64
+import folium
 
-class mapomatic(Map):
+class Mapomatic(Map):
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.markers = []
@@ -21,35 +25,146 @@ class mapomatic(Map):
     def add_marker(self, marker):
         self.markers.append(marker)
         self.add_layer(marker)
+
+    def add_image(self, url, position, size, opacity=1.0, layer_name=None):
+        image = ImageOverlay(
+            url=url,
+            bounds=[position, (position[0] + size[0], position[1] + size[1])],
+            opacity=opacity,
+            name=layer_name
+        )
+        self.add_layer(image)
     
-    def add_shp(self, in_shp, **kwargs):
-        geo_data = gpd.read_file(in_shp)
-        geo_data = geo_data.set_crs(epsg=4326)
-        geo_data = geo_data.to_crs("EPSG:4326")
-        geo_json = GeoJSON(data=geo_data.__geo_interface__, name=kwargs.get('layer_name', 'Layer'))
-        geo_json.style = kwargs.get('style', {})
-        self.add_layer(geo_json)
+    def add_raster(self, cog_path, cmap='viridis', opacity=1.0, layer_name=None):
+        
+        with rasterio.open(cog_path) as src:
+            data = src.read(1, out_shape=(1, int(src.height), int(src.width)))
+            bounds = src.bounds
+            transform = src.transform
 
-    def add_legend(self, legend_title, legend_dict):
-        legend_items = []
-        for name, color in legend_dict.items():
-            legend_items.append((color, name))
+        # Normalize the data to be between 0 and 1
+        data_norm = (data - np.nanmin(data)) / (np.nanmax(data) - np.nanmin(data))
 
-        colormap = linear.LinearColormap(
-            colors=[color for color in legend_dict.values()],
-            index=[0, 1],
-            vmin=0,
-            vmax=1
+        # Convert the data to RGBA using the specified colormap
+        cmap = plt.get_cmap(cmap)
+        data_rgba = (cmap(data_norm) * 255).astype(np.uint8)
+        data_rgba[:, :, 3] = (np.array(opacity) * 255).astype(np.uint8)
+
+        # Ensure that the array is C-contiguous
+        data_rgba = np.ascontiguousarray(data_rgba)
+
+        # Create a new TileLayer from the COG data
+        tile_layer = TileLayer(
+            url='data:image/tiff;base64,' + base64.b64encode(data_rgba).decode('utf-8'),
+            bounds=((bounds.bottom, bounds.left), (bounds.top, bounds.right)),
+            opacity=opacity,
+            name=layer_name
         )
 
-        legend_control = LegendControl(
-            position="bottomright",
-            title=legend_title,
-            colors=[color for color in legend_dict.values()],
-            labels=[name for name in legend_dict.keys()],
-            colormap=colormap,
-        )
-        self.add_control(legend_control)
+        # Add the new TileLayer to the map
+        self.add_layer(tile_layer)
+    
+    def add_basemap(self, basemap_name, url_template):
+        """
+        Adds a basemap to the map using a URL template.
+        
+        Parameters:
+        basemap_name (str): The name of the basemap to add.
+        url_template (str): The URL template to use for the new basemap layer. Must be 
+            a valid XYZ tile service.
+        """
+        # Remove the default OpenStreetMap basemap layer, if present
+        if len(self.layers) > 1:
+            self.remove_layer(self.layers[0])
+        
+        # Add the new basemap layer
+        new_layer = TileLayer(url=url_template, attribution=basemap_name)
+        self.add_layer(new_layer)
+    
+    def add_shp(self, shp_path):
+        """
+        Adds a shapefile to the map. 
+        
+        Parameters:
+        shp_path (str): The file path or HTTP URL to the shapefile in a zip file.
+        """
+        # If the path is an HTTP URL, download and unzip the shapefile
+        if shp_path.startswith('http'):
+            response = requests.get(shp_path)
+            with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
+                zip_ref.extractall()
+            shp_path = shp_path.split('/')[-1].split('.')[0] + '.shp'
+        
+        # Read the shapefile using GeoPandas
+        gdf = gpd.read_file(shp_path)
+        
+        # Convert the GeoDataFrame to a GeoJSON object
+        geojson = GeoJSON(data=gdf.__geo_interface__)
+        
+        # Add the GeoJSON layer to the map
+        self.add_layer(geojson)
+        
+        # Add a layer control to the map
+        control = LayersControl(position='topright')
+        self.add_control(control)
+    
+    def add_geojson(self, geojson_path):
+        """
+        Adds a GeoJSON file to the map. 
+        
+        Parameters:
+        geojson_path (str or dict): The file path or dictionary object containing GeoJSON data.
+        """
+        # If the path is an HTTP URL, download the GeoJSON file
+        if isinstance(geojson_path, str) and geojson_path.startswith('http'):
+            response = requests.get(geojson_path)
+            geojson_data = response.json()
+        # Otherwise, assume it's a file path or a dictionary object
+        else:
+            with open(geojson_path) as f:
+                geojson_data = json.load(f)
+        
+        # Create a GeoJSON layer and add it to the map
+        geojson = GeoJSON(data=geojson_data)
+        self.add_layer(geojson)
+        
+        # Add a layer control to the map
+        control = LayersControl(position='topright')
+        self.add_control(control)
+    def add_vector(self, vector_data):
+        """
+        Adds a vector data to the map. The vector data can be in any GeoPandas-supported
+        format, such as GeoJSON, shapefile, GeoDataFrame, etc.
+        
+        Parameters:
+        vector_data (str or dict or GeoDataFrame): The vector data to add to the map. 
+            Can be a file path or URL to the vector data, a dictionary object containing 
+            GeoJSON data, or a GeoDataFrame.
+        """
+        # If the vector data is a file path or URL, read it using GeoPandas
+        if isinstance(vector_data, str):
+            try:
+                gdf = gpd.read_file(vector_data)
+            except ValueError:
+                gdf = gpd.read_file(vector_data, encoding='utf-8')
+        # If the vector data is a dictionary object, create a GeoDataFrame
+        elif isinstance(vector_data, dict):
+            gdf = gpd.GeoDataFrame.from_features(vector_data['features'])
+        # If the vector data is already a GeoDataFrame, use it directly
+        elif isinstance(vector_data, gpd.GeoDataFrame):
+            gdf = vector_data
+        else:
+            raise ValueError('Invalid vector data format. Must be a file path or URL, a dictionary object containing GeoJSON data, or a GeoDataFrame.')
+        
+        # Convert the GeoDataFrame to a GeoJSON object
+        geojson = GeoJSON(data=gdf.__geo_interface__)
+        
+        # Add the GeoJSON layer to the map
+        self.add_layer(geojson)
+        
+        # Add a layer control to the map
+        control = LayersControl(position='topright')
+        self.add_control(control)
 
 locations = {}
 
@@ -98,28 +213,3 @@ def generate_input_points():
 
             except ValueError as e:
                 print(f"Invalid input: {e}")
-
-    with open('locations.csv', mode='w', newline='') as csv_file:
-        fieldnames = ['name', 'lat', 'lon']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for name, coords in locations.items():
-            writer.writerow({'name': name, 'lat': coords['lat'], 'lon': coords['lon']})
-    df = pd.read_csv('locations.csv')
-
-    geometry = [Point(xy) for xy in zip(df['lon'], df['lat'])]
-    gdf = gpd.GeoDataFrame(df, geometry=geometry)
-    gdf.to_file('locations.shp', driver='ESRI Shapefile')
-
-    colors = {}
-    for i, name in enumerate(gdf['name']):
-        colors[name] = f"#{i+1:02x}0000"
-
-    style = {'fillOpacity': 0.7, 'weight': 1, 'color': 'black'}
-    m = mapomatic(center=[0, 0], zoom=2)
-    in_shp = 'locations.shp'
-    m.add_shp(in_shp, layer_name="points", style=style)
-    
-    from IPython.display import display
-    display(m)
