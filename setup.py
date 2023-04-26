94bd88c82fcf1687d5645a75de677874684841f9
Bass Neel <maps.bassneel@gmail.com>
2023-04-26 01:25:15 -0400

    fixing issue with bumpversion

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bassneel/bassmap',
-    version='0.0.7',
+    version='0.0.9',
     zip_safe=False,
 )
